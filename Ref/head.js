5cb2ed4c91d19a45ef07e9bb5eec0b7aef14f7a2
DingleDangleDoff <82572801+DingleDangleDoff@users.noreply.github.com>
2026-04-12 15:08:58 +1000

    Added tail section to ref
    Also deleted useless things

--- Ref/head.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("Switch").addEventListener("change", function() {
-    let img = document.getElementById("Head Shape 2");
-
-    if (this.checked) {
-        img.src = "/Ref/images/Head Shape 2.5.png";
-    } else {
-        img.src = "/Ref/images/Head Shape 2.png";
-    }
-});
